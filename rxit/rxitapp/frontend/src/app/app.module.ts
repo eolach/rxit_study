5eef6c0d52becc2b91a57f72ee7775fcdfff1d78
Neil McEvoy <neil@eolas.ca>
2018-11-11 16:38:11 -0500

    Changed banner

--- rxit/rxitapp/frontend/src/app/app.module.ts
+++ rxit/rxitapp/frontend/src/app/app.module.ts
@@ -1,23 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { CustomMaterialModule } from './core/material.module';
+import { AppRoutingModule } from './core/app.routing.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { DispenserComponent } from './dispenser/dispenser.component';
+import { PrescriberComponent } from './prescriber/prescriber.component';
 
-import {MaterialModule} from './material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    DispenserComponent,
+    PrescriberComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    FormsModule,
+    CustomMaterialModule,
+    AppRoutingModule,
     FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
