0f65c0197f6e38efbb534c230df6274458a47a4c
Neil McEvoy <neil@eolas.ca>
2018-11-25 09:45:41 -0500

    Fixed spelling of development in wsgi.py

--- rxit/rxit/wsgi.py
+++ rxit/rxit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rxit.settings.developemnt')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rxit.settings.development')
 
 application = get_wsgi_application()
