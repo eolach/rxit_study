070a5d9cdda1a1eca36bfa3cb34a214f4a37aa14
Neil McEvoy <neil@eolas.ca>
2018-11-25 09:54:43 -0500

    Temporary secret key

--- rxit/rxit/settings/staging.py
+++ rxit/rxit/settings/staging.py
@@ -1,8 +1,9 @@
 from rxit.settings.common import *
 import os
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-
+# 
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = '#ym%cn@shk8d-ya_f6$mk$@io&-gehdc$vo8)gx5q)9(=qjqu&'
 DEBUG = True
 
 ALLOWED_HOSTS = [
