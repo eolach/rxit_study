b1bc90513d6573efdfcf34bbaa29324676f1d07a
Neil McEvoy <neil@eolas.ca>
2018-12-13 08:00:37 -0500

    Removed older folders Looking for fresh rest_framework static files

--- rxit/rxitapp/frontend/src/app/user/user.model.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-        username: string;
-        participant_name: string;
-        password: string;
-        participant_type: string;
-        participant_index: number;
-}
