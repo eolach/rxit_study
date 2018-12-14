df9ef809079c72011650b7c6ada9427e99c9df04
Neil McEvoy <neil@eolas.ca>
2018-12-14 15:01:21 -0500

    Environment variables bypassed for now

--- rxit2_dir/rxit_2_project/rxit_2/settings.py
+++ rxit2_dir/rxit_2_project/rxit_2/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY='cd48e1c22de0961d5d1bfb14f8a66e006cfb1cfbf3f0c0f3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+# DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
