02e8d830e6001b8d14c1eefb3d1ec0fc2b6719af
Neil McEvoy <neil@eolas.ca>
2018-12-18 18:28:00 -0500

    Extended both models (incomplete)

--- rxit2_dir/rxit_2_project/rxit_app/participants/dispenser.py
+++ rxit2_dir/rxit_2_project/rxit_app/participants/dispenser.py
@@ -4,21 +4,9 @@ from drf_writable_nested import WritableNestedModelSerializer
 
 # Field sets for dispenser
 
-class Dispenser(models.Model):
-    username = models.CharField(max_length=100, blank=True, default='')
 
-    def __str__(self):
-        return self.username
-
-
-"""
 # Description
-class Description(models.Model):
-    dispenser = models.OneToOneField(
-        Dispenser,
-        on_delete=models.CASCADE,
-    ) 
-       
+class RxDescription(models.Model):
     participant_name = models.CharField(max_length=100, blank=True, default='')
     street = models.CharField(max_length=100, blank=True, default='') 
     city = models.CharField(max_length=100, blank=True, default='')
@@ -26,36 +14,176 @@ class Description(models.Model):
     corporate_type = models.CharField(max_length=100, blank=True, default='')
     pharmacy_mgt_system = models.CharField(max_length=100, blank=True, default='')
 
- # RxStats
-class RxStats(models.Model):
-    dispenser = models.OneToOneField(
-        Dispenser,
-        on_delete=models.CASCADE,
-    ) 
-    num_am = models.DecimalField(max_digits=4, decimal_places=1, default=0)
-    num_pm = models.DecimalField(max_digits=4, decimal_places=1, default=0)
-    num_evng = models.DecimalField(max_digits=4, decimal_places=1, default=0)
-    num_wend = models.DecimalField(max_digits=4, decimal_places=1, default=0) 
-
 # Numbers
 class Numbers(models.Model):
-    dispenser = models.OneToOneField(
-        Dispenser,
-        on_delete=models.CASCADE,
-    ) 
     num_pharmacists = models.DecimalField(max_digits=3, decimal_places=1, default=0)
     num_reg_tech = models.DecimalField(max_digits=3, decimal_places=1, default=0)
     num_unreg = models.DecimalField(max_digits=3, decimal_places=1, default=0)
 
+    num_am = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+# RxStats
+class RxStats(models.Model):
+    num_am = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    num_pm = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    num_evng = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    num_wend = models.DecimalField(max_digits=4, decimal_places=1, default=0) 
+    stats_notes = models.CharField(max_length=300, blank=True, default='')
 
-# TxTime
-# TxRole
+# RxProcess
+class RxProcess(models.Model):
+    num_new_pt = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    num_new_rx = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    num_rpt_rx = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    pharm_ip = models.BooleanField(default=False)
+    reg_ip = models.BooleanField(default=False)
+    unreg_ip = models.BooleanField(default=False)
+
 # RxReview
+class RxReview(models.Model):
+    before_rx = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    after_rx = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+ 
 # Communication
-"""
+class RxComm(models.Model):
+    daily_duration = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    daily_freq = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    daily_elapsed = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    by_fax = models.BooleanField(default=False)
+    by_phone = models.BooleanField(default=False)
+    by_dm = models.BooleanField(default=False)
+
+class Dispenser(models.Model):
+    description = models.OneToOneField(
+        RxDescription,
+        on_delete=models.CASCADE,
+    )
+    numbers = models.OneToOneField(
+        Numbers,
+        on_delete=models.CASCADE,
+    ) 
+    total_rx = models.ForeignKey(
+        RxStats,
+        related_name='total_rx',
+        on_delete=models.CASCADE,
+    ) 
+    walkin_rx = models.ForeignKey(
+        RxStats,
+        related_name='walkin_rx',
+        on_delete=models.CASCADE,
+    ) 
+    faxed_rx = models.ForeignKey(
+        RxStats,
+        related_name='faxed_rx',
+        on_delete=models.CASCADE,
+    ) 
+    e_prescribe_rx = models.ForeignKey(
+        RxStats,
+        related_name='e_prescribe_rx',
+        on_delete=models.CASCADE,
+    ) 
+    phoned_rx = models.ForeignKey(
+        RxStats,
+        related_name='phoned_rx',
+        on_delete=models.CASCADE,
+    ) 
+    rx_process = models.OneToOneField(
+        RxProcess,
+        on_delete=models.CASCADE,
+    ) 
+    review_new_pt = models.ForeignKey(
+        RxReview,
+        related_name='review_new_pt',
+        on_delete=models.CASCADE,
+    ) 
+    review_new_rx = models.ForeignKey(
+        RxReview,
+        related_name='review_new_rx',
+        on_delete=models.CASCADE,
+    ) 
+    review_rpt_rx = models.ForeignKey(
+        RxReview,
+        related_name='review_rpt_pt',
+        on_delete=models.CASCADE,
+    ) 
+    comm_illegible = models.ForeignKey(
+        RxComm,
+        related_name='comm_illegible',
+        on_delete=models.CASCADE,
+    ) 
+    comm_incomplete = models.ForeignKey(
+        RxComm,
+        related_name='comm_incomplete',
+        on_delete=models.CASCADE,
+    ) 
+    comm_dose = models.ForeignKey(
+        RxComm,
+        related_name='comm_dose',
+        on_delete=models.CASCADE,
+    ) 
+    comm_advise = models.ForeignKey(
+        RxComm,
+        related_name='comm_advise',
+        on_delete=models.CASCADE,
+    ) 
+    comm_renewal = models.ForeignKey(
+        RxComm,
+        related_name='comm_renewal',
+        on_delete=models.CASCADE,
+    ) 
+    comm_cancel = models.ForeignKey(
+        RxComm,
+        related_name='comm_cancel',
+        on_delete=models.CASCADE,
+    ) 
+    comm_consult = models.ForeignKey(
+        RxComm,
+        related_name='comm_consult',
+        on_delete=models.CASCADE,
+    ) 
+    
+
+    username = models.CharField(max_length=100, blank=True, default='')
+    stats_notes = models.CharField(max_length=300, blank=True, default='')
+    review_notes = models.CharField(max_length=300, blank=True, default='')
+    def __str__(self):
+        return self.username
+
+
 # Serializers
 
-""" class RxStatsSerializer(serializers.ModelSerializer):
+class RxProcessSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RxProcess
+        fields = ('pk',
+            'num_new_pt',
+            'num_new_rx',
+            'num_rpt_rx',
+            'pharm_ip',
+            'reg_ip',
+            'unreg_ip'
+        )
+class RxReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RxReview
+        fields = ('pk',
+            'before_rx',
+            'after_rx'
+        )
+class RxCommSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RxComm
+        fields = ('pk',
+            'daily_duration',
+            'daily_freq',
+            'daily_elapsed',
+            'by_fax',
+            'by_phone',
+            'by_dm'
+        )
+class RxStatsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = RxStats
@@ -74,11 +202,10 @@ class NumbersSerializer(serializers.ModelSerializer):
             'num_reg_tech',
             'num_unreg'
             )
-
-class DescriptionSerializer(serializers.ModelSerializer):
+class RxDescriptionSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Description
+        model = RxDescription
         fields = ('pk',
             'participant_name',
             'street',
@@ -87,22 +214,53 @@ class DescriptionSerializer(serializers.ModelSerializer):
             'corporate_type', 
             'pharmacy_mgt_system'
         )
-"""
 
 class DispenserSerializer(WritableNestedModelSerializer):
 
-    # description = DescriptionSerializer()
-    # numbers = NumbersSerializer()
-    # total_rx = RxStatsSerializer()
-    # walk_in_rx = RxStatsSerializer()
+    description = RxDescriptionSerializer()
+    numbers = NumbersSerializer()
+    total_rx = RxStatsSerializer()
+    walkin_rx = RxStatsSerializer()
+    faxed_rx = RxStatsSerializer()
+    e_prescribe_rx = RxStatsSerializer()
+    phoned_rx = RxStatsSerializer()
+    rx_process = RxProcessSerializer()
+    review_new_pt = RxReviewSerializer()
+    review_new_rx = RxReviewSerializer()
+    review_rpt_rx = RxReviewSerializer()
+    comm_illegible = RxCommSerializer()
+    comm_incomplete = RxCommSerializer()
+    comm_dose = RxCommSerializer()
+    comm_advise = RxCommSerializer()
+    comm_renewal = RxCommSerializer()
+    comm_cancel = RxCommSerializer()
+    comm_consult = RxCommSerializer()
+
 
     class Meta:
         model = Dispenser
         fields = ('pk',
             'username',
-            # 'description',
-            # 'numbers',
-            # 'total_rx'
+            'description',
+            'numbers',
+            'total_rx',
+            'walkin_rx',
+            'faxed_rx',
+            'e_prescribe_rx',
+            'phoned_rx',
+            'stats_notes',
+            'rx_process',
+            'review_new_pt',
+            'review_new_rx',
+            'review_rpt_rx',
+            'review_notes',
+            'comm_illegible',
+            'comm_incomplete',
+            'comm_dose',
+            'comm_advise',
+            'comm_renewal',
+            'comm_cancel',
+            'comm_consult'
         )
 
             
--- rxit2_dir/rxit_2_project/rxit_app/participants/prescriber.py
+++ rxit2_dir/rxit_2_project/rxit_app/participants/prescriber.py
@@ -1,29 +1,254 @@
 from django.db import models
 from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
 
-
-class Prescriber(models.Model):
-    username = models.CharField(max_length=100, blank=True, default='')
+# Description
+class DxDescription(models.Model):
     participant_name = models.CharField(max_length=100, blank=True, default='')
     street = models.CharField(max_length=100, blank=True, default='') 
     city = models.CharField(max_length=100, blank=True, default='')
     province = models.CharField(max_length=100, blank=True, default='')
     practice_type = models.CharField(max_length=100, blank=True, default='')
+    medical_record_system = models.CharField(max_length=100, blank=True, default='')
+    num_physicians = models.DecimalField(max_digits=3, decimal_places=1, default=0)
+    
+    FHT_FHN_FHO = 'FH'
+    GROUP = 'GP'
+    SOLO = 'SL'
+    PRACTICE_CHOICES = (
+        (FHT_FHN_FHO, 'FHT?FHN?FHO'),
+        (GROUP, 'Solo'),
+        (SOLO, 'Junior')
+    )
+    practice_type = models.CharField(
+        max_length=2,
+        choices=PRACTICE_CHOICES,
+        default=SOLO,
+    )
+
+# DxStats
+class DxStats(models.Model):
+    num_daily = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    num_weeky = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+
+class DxDelivery(models.Model):
+    method_del = models.BooleanField(default=False)
+    fraction_del = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+
+class DxAdmin(models.Model):
+    daily_freq = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    daily_rx_messages = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    pc_urgent_messages = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    total_time_messages = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+
+    ADMIN = 'AD'
+    ASST = 'AS'
+    NURSE = 'RN'
+    PHYSICIAN = 'MD'
+    OTHER = 'OT'
+    ACTIVITY_CHOICES = (
+        (ADMIN, 'Office admin'),
+        (ASST, 'Assistant'),
+        (NURSE, 'Nurse'),
+        (PHYSICIAN, 'Physician'),
+        (OTHER, 'Other')
+    )
+    comm_role = models.CharField(
+        max_length=2,
+        choices=ACTIVITY_CHOICES,
+        default=ADMIN,
+    )
+
+class DxPrep(models.Model):
+    dx_freq = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    dx_duration = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    within_emr = models.BooleanField(default=False)
+    linked_to_emr = models.BooleanField(default=False)
+    desktop = models.BooleanField(default=False)
+    mobile_app = models.BooleanField(default=False)
+
+class DxSpec(models.Model):
+     spec_duration = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+     by_hand = models.BooleanField(default=False)
+     free_text = models.BooleanField(default=False)
+     drop_down = models.BooleanField(default=False)
+     check_box = models.BooleanField(default=False)
+     search = models.BooleanField(default=False)
+  
+
+class Prescriber(models.Model):
+    description = models.OneToOneField(
+        DxDescription,
+        on_delete=models.CASCADE,
+    ) 
+    total_pts = models.ForeignKey(DxStats, related_name='total_pts', on_delete=models.CASCADE,) 
+    std_pts = models.ForeignKey(DxStats, related_name='std_pts', on_delete=models.CASCADE,) 
+    extend_pts = models.ForeignKey(DxStats, related_name='extend_pts', on_delete=models.CASCADE,) 
+    ongoing_pts = models.ForeignKey(DxStats, related_name='ongoing_pts', on_delete=models.CASCADE,) 
+    total_rx = models.ForeignKey(DxStats, related_name='total_rx', on_delete=models.CASCADE,) 
+    new_rx = models.ForeignKey(DxStats, related_name='new_rx', on_delete=models.CASCADE,) 
+    renew_rx = models.ForeignKey(DxStats, related_name='renew_rx', on_delete=models.CASCADE,) 
+    auto_renew_rx = models.ForeignKey(DxStats, related_name='auto_renew_rx', on_delete=models.CASCADE,) 
+    poly_rx = models.ForeignKey(DxStats, related_name='poly_rx', on_delete=models.CASCADE,) 
+    clarify_msg = models.ForeignKey(DxStats, related_name='clarify_msg', on_delete=models.CASCADE,) 
+    authorize_msg = models.ForeignKey(DxStats, related_name='authorize_msg', on_delete=models.CASCADE,) 
+    stats_notes = models.CharField(max_length=100, blank=True, default='')
+
+    printed_rx = models.ForeignKey(DxDelivery, related_name='printed_rx', on_delete=models.CASCADE,) 
+    faxed_rx = models.ForeignKey(DxDelivery, related_name='faxed_rx', on_delete=models.CASCADE,) 
+    phoned_rx = models.ForeignKey(DxDelivery, related_name='phoned_rx', on_delete=models.CASCADE,) 
+    e_prescribe_rx = models.ForeignKey(DxDelivery, related_name='e_prescribe_rx', on_delete=models.CASCADE,) 
+
+    receive_msg = models.ForeignKey(DxAdmin, related_name='receive_msg', on_delete=models.CASCADE,) 
+    process_msg = models.ForeignKey(DxAdmin, related_name='process_msg', on_delete=models.CASCADE,) 
+
+    pat_hx = models.ForeignKey(DxPrep, related_name='pat_hx', on_delete=models.CASCADE,) 
+    cds = models.ForeignKey(DxPrep, related_name='cds', on_delete=models.CASCADE,) 
+    p_formulary = models.ForeignKey(DxPrep, related_name='p_formulary', on_delete=models.CASCADE,) 
+    p_dis = models.ForeignKey(DxPrep, related_name='p_dis', on_delete=models.CASCADE,) 
+
+    drug_name = models.ForeignKey(DxSpec, related_name='drug_name', on_delete=models.CASCADE,) 
+    dosage = models.ForeignKey(DxSpec, related_name='dosage', on_delete=models.CASCADE,) 
+    refills = models.ForeignKey(DxSpec, related_name='refills', on_delete=models.CASCADE,) 
+    route = models.ForeignKey(DxSpec, related_name='route', on_delete=models.CASCADE,) 
+    instructions = models.ForeignKey(DxSpec, related_name='instructions', on_delete=models.CASCADE,) 
+ 
+    username = models.CharField(max_length=100, blank=True, default='')
+    deliver_notes = models.CharField(max_length=100, blank=True, default='')
 
     def __str__(self):
         return self.username
 
-
-
-class PrescriberSerializer(serializers.ModelSerializer):
- 
+class DxStatsSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Prescriber
-        fields = ('id', 
-            'username',
+        model = DxStats
+        fields = ('pk',
+            'num_daily',
+            'num_weeky'
+        )
+
+class DxDeliverySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DxDelivery
+        fields = ('pk',
+            'method_del',
+            'fraction_del'
+        )
+
+class DxAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DxAdmin
+        fields = ('pk',
+            'daily_freq',
+            'daily_rx_messages',
+            'pc_urgent_messages',
+            'total_time_messages',
+            'comm_role'
+        )
+
+class DxPrepSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DxPrep
+        fields = ('pk',
+            'dx_freq',
+            'dx_duration',
+            'within_emr',
+            'linked_to_emr',
+            'desktop',
+            'mobile_app'
+        )
+
+class DxSpecSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DxSpec
+        fields = ('pk',
+            'spec_duration',
+            'by_hand',
+            'free_text',
+            'drop_down',
+            'check_box',
+            'search'
+        )
+
+class DxDescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DxDescription
+        fields = ('pk',
             'participant_name', 
             'street',
             'city',
             'province',
+            'practice_type',
+            'medical_record_system',
+            'num_physicians',
             'practice_type'
+        )
+
+class PrescriberSerializer(WritableNestedModelSerializer):
+ 
+    description = DxDescriptionSerializer()
+    total_pts = DxStatsSerializer()
+    std_pts = DxStatsSerializer()
+    extend_pts = DxStatsSerializer()
+    ongoing_pts = DxStatsSerializer()
+    total_rx = DxStatsSerializer()
+    new_rx = DxStatsSerializer()
+    renew_rx = DxStatsSerializer()
+    auto_renew_rx = DxStatsSerializer()
+    poly_rx = DxStatsSerializer()
+    clarify_msg = DxStatsSerializer()
+    authorize_msg = DxStatsSerializer()
+
+    printed_rx =  DxDeliverySerializer()  
+    faxed_rx =  DxDeliverySerializer()  
+    phoned_rx =  DxDeliverySerializer()  
+    e_prescribe_rx =  DxDeliverySerializer()  
+
+    receive_msg = DxAdminSerializer()
+    process_msg = DxAdminSerializer()
+
+    pat_hx = DxPrepSerializer()
+    cds = DxPrepSerializer()
+    p_formulary = DxPrepSerializer()
+    p_dis = DxPrepSerializer()
+
+    drug_name = DxSpecSerializer()
+    dosage = DxSpecSerializer()
+    refills = DxSpecSerializer()
+    route = DxSpecSerializer()
+    instructions = DxSpecSerializer()
+
+    class Meta:
+        model = Prescriber
+        fields = ('pk', 
+            'username',
+            'description',
+            'total_pts',
+            'std_pts',
+            'extend_pts',
+            'ongoing_pts',
+            'total_rx',
+            'new_rx',
+            'renew_rx',
+            'auto_renew_rx',
+            'poly_rx',
+            'clarify_msg',
+            'authorize_msg',
+            'stats_notes',
+            'printed_rx',
+            'faxed_rx',
+            'phoned_rx',
+            'e_prescribe_rx',
+            'deliver_notes',
+            'receive_msg',
+            'process_msg',
+            'pat_hx',
+            'cds',
+            'p_formulary',
+            'p_dis',
+            'drug_name',
+            'dosage',
+            'refills',
+            'route',
+            'instructions'
             )
