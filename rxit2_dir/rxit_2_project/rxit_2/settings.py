e825155e4f6a23534487a0b0fb57a6e2f46e7803
eolach <neil@eolas.ca>
2019-05-06 18:56:24 +0000

    prior to pulling remote

--- rxit2_dir/rxit_2_project/rxit_2/settings.py
+++ rxit2_dir/rxit_2_project/rxit_2/settings.py
@@ -149,10 +149,7 @@ STATICFILES_DIRS = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-
-
-
-
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
